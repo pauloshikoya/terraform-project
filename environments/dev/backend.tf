a2dde6eaa80fd0c962675a551971688d119ace90
kolawole oshikoya <Kolawole.Oshikoya@santander.co.uk>
2026-01-29 13:17:17 +0000

    Initialisation

--- environments/dev/backend.tf
+++ environments/dev/backend.tf
@@ -1,10 +1,10 @@
 terraform {
   backend "s3" {
-    bucket = "terraform-platform-state-paul"
-    key    = "dev/terraform.tfstate"
-    region = "eu-west-2"
+    bucket         = "terraform-platform-state-paul"
+    key            = "dev/terraform.tfstate"
+    region         = "eu-west-2"
     dynamodb_table = "terraform-locks"
-    encrypt      = true
+    encrypt        = true
     # use_lockfile = true
   }
 }
